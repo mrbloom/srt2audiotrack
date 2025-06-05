4cb6eacab6cf2d917ca525fd4a7d41c94fb5e1f9
mrbloom <mrbloom@ukr.net>
2025-06-05 17:32:37 +0300

    refactoring 17:32

--- separate.py
+++
@@ -1,7 +0,0 @@
-import demucs.separate
-
-def separate_audio():
-    demucs.separate.main(["--jobs", "4","-o", "records\\one_voice\\separated", "--two-stems", "vocals", "-n", "mdx_extra", "records\\one_voice\\tanks_en.mp4"])
-
-if __name__ == "__main__":
-    separate_audio()
